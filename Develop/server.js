f34df4ba70d01504fdda9359e742ba36bcb6891e
Brett Miller <brett.miller47@yahoo.com>
2022-09-07 11:22:19 -0400

    initial commit

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 
-const data = require('./db/db.json');
+const notes = require('./db/db.json');
 
 const PORT = 3001;
 const app = express();
@@ -17,15 +17,37 @@ app.get('/', (req, res) => {
 
 // GET /notes should return the notes.html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // GET /api/notes should return the db.json file and return all saved notes as JSON
-app.get('/api/notes', (req, res) => res.json(data));
+app.get('/api/notes', (req, res) => res.json(notes));
 
 // POST /api/notes should receive a new note to save on the request body, 
 // -- add it to the db.json file, and then return the new note to the client.
+app.post('/notes', (req, res) => {
 
+    const { title, text } = req.body;
+
+    if (title && text) {
+        
+        const newFeedback = {
+            title,
+            text
+        };
+        
+        readAndAppend(newFeedback, '/db/db.json');
+        
+        const response = {
+            status: 'success', 
+            body: newFeedback
+        }
+        res.json(response);
+  } else {
+        res.json('Request body must contain a title and text.');
+  }
+
+});
 
 app.listen(PORT, () => {
     console.log(`listening on PORT http://localhost:${PORT}`);
