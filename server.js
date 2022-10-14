c104bbd87fd269b4357bf0d08856f4a1de9e9847
Brett Miller <brett.miller47@yahoo.com>
2022-10-14 10:54:42 -0400

    pushing untracked changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const shortid = require('shortid')
+const util = require('util');
 
 const notes = require('./db/db.json');
 
@@ -16,20 +17,42 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// GET /notes should return the notes.html
-// ! WHY DOESN'T THIS WORK, IT'S THE SAME AS ABOVE?!
+// ! GET /notes should return the notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-// GET /api/notes should return the db.json file and return all saved notes as JSON
-app.get('/api/notes', (req, res) => res.json(notes));
 
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+  
+// GET /api/notes should return the db.json file and return all saved notes as JSON
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 // POST /api/notes should receive a new note to save on the request body, 
 // -- add it to the db.json file, and then return the new note to the client.
 app.post('/api/notes', (req, res) => {
 
-    const { title, text, note } = req.body;
+    const { title, text} = req.body;
 
     if (title && text) {
         const newFeedback = {
@@ -38,13 +61,11 @@ app.post('/api/notes', (req, res) => {
             note_id: shortid.generate()
         };
 
-        // ! WHY DOESN'T THIS WORK?!
         readAndAppend(newFeedback, '/db/db.json');
-        
         const response = {
-            status: 'success', 
-            body: newFeedback
-        }
+            status: 'success',
+            body: newFeedback,
+        };
         res.json(response);
   } else {
         res.json('Request body must contain a title and text.');
